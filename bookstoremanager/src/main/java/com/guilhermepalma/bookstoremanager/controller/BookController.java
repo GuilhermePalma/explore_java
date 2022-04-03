141acbd68edf42ecfebeaee310f8efd478385feb
Guilherme <guilherme.palma8@etec.sp.gov.br>
2022-04-03 17:25:34 -0300

    Refactor: Separa Responsibilidade entre o Controller e Service

--- bookstoremanager/src/main/java/com/guilhermepalma/bookstoremanager/controller/BookController.java
+++ bookstoremanager/src/main/java/com/guilhermepalma/bookstoremanager/controller/BookController.java
@@ -3,6 +3,7 @@ package com.guilhermepalma.bookstoremanager.controller;
 import com.guilhermepalma.bookstoremanager.dto.MessageResponseDTO;
 import com.guilhermepalma.bookstoremanager.entity.Book;
 import com.guilhermepalma.bookstoremanager.repository.BookRepository;
+import com.guilhermepalma.bookstoremanager.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,29 +11,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Essa Classe Representa um Controlador de uma API REST, com o endpoint "/api/b1/books")
+ * Essa Classe Representa um Controlador de uma API REST, com o endpoint "/api/b1/books"
+ * que recebe os dados que serão utilizados.
  */
 @RestController // Anotação para representar uma classe de Controlador REST API
 @RequestMapping("/api/v1/books") // Define a URL de Acesso (nomesite.com/api/v1/books)
 public class BookController {
 
-    // Classe de Consultas no Banco de Dados de Livros
-    private final BookRepository bookRepository;
+    private final BookService bookService;
 
     @Autowired // Responsavel pela injeção de dependencia para usar uma Classe na classe Atual
-    public BookController(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
     }
 
     @PostMapping // Define que será uma solicitação POST
     public MessageResponseDTO create(@RequestBody Book book) {
-        // Salva e Obtem o Livro Instanciado (com o ID)
-        Book savedBook = bookRepository.save(book);
-
-        // Usando da Anotação Lambok, retorna uma Resposta com o ID do Livro Inserido
-        return MessageResponseDTO.builder()
-                .message("ID do Livro Inserido: " + savedBook.getId())
-                .build();
+        // A Classe Controller Recebe os Dados e a manipulação fica com a Classe Service
+        return bookService.create(book);
     }
 
 }
---
+++ bookstoremanager/src/main/java/com/guilhermepalma/bookstoremanager/service/BookService.java
@@ -0,0 +1,33 @@
+package com.guilhermepalma.bookstoremanager.service;
+
+import com.guilhermepalma.bookstoremanager.dto.MessageResponseDTO;
+import com.guilhermepalma.bookstoremanager.entity.Book;
+import com.guilhermepalma.bookstoremanager.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Classe responsavel pelas manipulações e regras de negocios dos dados dos Livros.
+ * É gerenciada pelo Spring, mas pode ser inserido outras dependencias no seu uso
+ */
+@Service
+public class BookService {
+
+    // Classe de Consultas no Banco de Dados de Livros
+    private final BookRepository bookRepository;
+
+    @Autowired // Responsavel pela injeção de dependencia para usar uma Classe na classe Atual
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public MessageResponseDTO create(Book book) {
+        // Salva e Obtem o Livro Instanciado (com o ID)
+        Book savedBook = bookRepository.save(book);
+
+        // Usando da Anotação Lambok, retorna uma Resposta com o ID do Livro Inserido
+        return MessageResponseDTO.builder()
+                .message("ID do Livro Inserido: " + savedBook.getId())
+                .build();
+    }
+}
