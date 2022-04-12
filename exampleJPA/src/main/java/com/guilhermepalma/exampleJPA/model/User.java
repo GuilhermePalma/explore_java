44758c5b36e26e55755efaeb528af0578ec59336
Guilherme <guilherme.palma8@etec.sp.gov.br>
2022-04-11 22:00:46 -0300

    Feature(exploreJPA): Adiciona Classe Usuario

---
+++ exampleJPA/src/main/java/com/guilhermepalma/exampleJPA/model/User.java
@@ -0,0 +1,51 @@
+package com.guilhermepalma.exampleJPA.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity // Diz que a Classse está relacionada à uma Classe "User" no Banco de Dados
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        super();
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
