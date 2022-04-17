10b60c6c00c64cf1c108d1db7989176871ff8411
Guilherme <guilherme.palma8@etec.sp.gov.br>
2022-04-16 23:04:32 -0300

    Feature(exploreJPA): Adiciona Herança com Tabela Geral usando JPA
    Inclui o Conceito de Herança usando o JPA, com a Estratégia "InheritanceType.SINGLE_TABLE". Nessa Estrategia a Classe Abstrata e todas as Classes que Herdam ficarão centralizadas em apenas uma unica Tabela no Banco de Dados

---
+++ exampleJPA/src/main/java/com/guilhermepalma/exampleJPA/model/inheritance/ScholarshipStudent.java
@@ -0,0 +1,27 @@
+package com.guilhermepalma.exampleJPA.model.inheritance;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("SS")
+public class ScholarshipStudent extends Student {
+
+    private Double valueScholarship;
+
+    public ScholarshipStudent() {
+    }
+
+    public ScholarshipStudent(Long id, String name, Double valueScholarship) {
+        super(id, name);
+        this.valueScholarship = valueScholarship;
+    }
+
+    public Double getValueScholarship() {
+        return valueScholarship;
+    }
+
+    public void setValueScholarship(Double valueScholarship) {
+        this.valueScholarship = valueScholarship;
+    }
+}
