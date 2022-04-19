b369638ff879b30447f19ce6c6e0a4ce974c54f3
Guilherme <guilherme.palma8@etec.sp.gov.br>
2022-04-19 20:19:53 -0300

    feat(exSpringBot): Adiciona Diferentes Metodos HTTP em Requisições

---
+++ exampleSpringBot/src/main/java/com/guilhermepalma/examplespringbot/controllers/MethodsHttpController.java
@@ -0,0 +1,33 @@
+package com.guilhermepalma.examplespringbot.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = {"/httpmethods", "/httpMethods"})
+public class MethodsHttpController {
+
+    @GetMapping
+    public String get() {
+        return "This is a GET Request";
+    }
+
+    @PostMapping
+    public String post() {
+        return "This is a POST Request";
+    }
+
+    @PutMapping
+    public String put() {
+        return "This is a PUT Request";
+    }
+
+    @PatchMapping
+    public String patch() {
+        return "This is a PATCH Request";
+    }
+
+    @DeleteMapping
+    public String delete() {
+        return "This is a DELETE Request";
+    }
+}
