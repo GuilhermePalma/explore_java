f3a31dd1204bbe07352e2a3fe486523850663973
Guilherme <guilherme.palma8@etec.sp.gov.br>
2022-04-27 22:25:29 -0300

    feat(exampleAxon): Adiciona Consulta de Participantes e Salas

---
+++ exampleAxonFramework/src/main/java/com/guilhermepalma/exampleaxonframework/query/rooms/participants/RoomParticipantsProjection.java
@@ -0,0 +1,42 @@
+package com.guilhermepalma.exampleAxonFramework.query.rooms.participants;
+
+import com.guilhermepalma.exampleAxonFramework.coreapi.PartipantJoinedRoomEvent;
+import com.guilhermepalma.exampleAxonFramework.coreapi.PartipantLeftRoomEvent;
+import com.guilhermepalma.exampleAxonFramework.coreapi.RoomParticipantQuery;
+import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class RoomParticipantsProjection {
+    private final RoomParticipantsRepository participantsRepository;
+
+    public RoomParticipantsProjection(RoomParticipantsRepository participantsRepository) {
+        this.participantsRepository = participantsRepository;
+    }
+
+    // QueryHandler: Responsavel por fazer a Manipulação das Consultas
+    @QueryHandler
+    public List<String> handler(RoomParticipantQuery participantQuery) {
+        return participantsRepository.findRoomParticipantsByRoomId(participantQuery.getRoomId())
+                .stream()
+                .map(RoomParticipants::getParticipant)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    @EventHandler
+    public void on(PartipantJoinedRoomEvent joinedRoomEvent) {
+        participantsRepository.save(new RoomParticipants(joinedRoomEvent.getRoomId(), joinedRoomEvent.getParticipant()));
+    }
+
+    @EventHandler
+    public void on(PartipantLeftRoomEvent leftRoomEvent) {
+        participantsRepository.deleteByParticipantAndRoomId(leftRoomEvent.getParticipant(), leftRoomEvent.getRoomId());
+    }
+
+
+}
---
+++ exampleAxonFramework/src/main/java/com/guilhermepalma/exampleaxonframework/query/rooms/sumary/RoomSumaryRepository.java
@@ -0,0 +1,6 @@
+package com.guilhermepalma.exampleAxonFramework.query.rooms.sumary;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomSumaryRepository extends JpaRepository<RoomSumary, String> {
+}
